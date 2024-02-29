566d0504d62d323d84e9795717491829295f825e
Øyvind Kristiansen <r3d5un@pm.me>
2024-02-29 14:05:51 +0100

    Add basic structural log statements

--- cmd/logging/main.go
+++ cmd/logging/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log/slog"
 	"os"
 )
@@ -10,5 +11,31 @@ func main() {
 	logger := slog.New(handler)
 	slog.SetDefault(logger)
 
+	// simple log statement
 	slog.Info("Hello, World!")
+
+	// log statement with extra attributes
+	slog.Info("extra attributes", "msg", "possibilities limitless")
+
+	// stronly typed attributes
+	slog.Info("strongly typed attributes", slog.String("msg", "possibilities limited"))
+
+	// guarantee strong types
+	slog.LogAttrs(
+		context.Background(), slog.LevelInfo,
+		"truly, the best choice is no choice",
+		slog.String("msg", "Do you have more microservices than customers?"),
+		slog.Bool("answer", true),
+	)
+
+	// grouping attributes
+	slog.LogAttrs(
+		context.Background(), slog.LevelInfo,
+		"these are not the droids you're looking for",
+		slog.Group(
+			"droids",
+			slog.Int("droid_id", 1),
+			slog.Int("droid_id", 2),
+		),
+	)
 }
